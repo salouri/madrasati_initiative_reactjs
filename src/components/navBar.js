98de40cd159f0be17d575fbf47db2e5a8c869b4e
Salouri Developer <salouri@gmail.com>
2021-10-12 02:06:18 +0300

    add functionality of auto setting active 'navitem' once clikced

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
-import Navitem from './Navitem';
+import Navitem from './navitem';
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeNavItem: '',
+    };
+  }
+
+  activateItem = (id) => {
+    // remove 'active' class from all "Navitem" DOM elements
+    if (this.state.navItemId.length > 0) {
+      document
+        .querySelector(`#${this.state.activeNavItem}`)
+        .classList.remove('active');
+    }
+
+    // add 'active' class to the id stored in the 'activeNavItem' state above
+    this.setState({ activeNavItem: id }, () => {
+      document.querySelector(`#${this.state.activeNavItem}`).classList.add('active');
+    });
+  };
+
   render() {
     return (
       <nav>
         <ul>
-          <Navitem item='Register' tolink='/'></Navitem>
-          <Navitem item='Search' tolink='/search'></Navitem>
-          <Navitem item='List' tolink='/list'></Navitem>
+          <Navitem
+            item='Register'
+            tolink='/'
+            activeFunc={this.activateItem}
+          ></Navitem>
+
+          <Navitem
+            item='Search'
+            tolink='/search'
+            activeFunc={this.activateItem}
+          ></Navitem>
+
+          <Navitem
+            item='List'
+            tolink='/list'
+            activeFunc={this.activateItem}
+          ></Navitem>
         </ul>
       </nav>
     );
