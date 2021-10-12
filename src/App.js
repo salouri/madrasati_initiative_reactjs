eb1748cd35659bcdeef0da88b8df41776684433e
Salouri Developer <salouri@gmail.com>
2021-10-13 01:18:49 +0300

    add all three routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
 import Navbar from './components/navBar';
 import Register from './contents/register';
+import List from './contents/list';
+import Search from './contents/search';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div className='App'>
         <Navbar />
-
+        {/* Route for register.js contents */}
         <Route exact path='/'>
           <Register />
+        </Route>
+
+        {/* Route for list.js contents */}
+        <Route path='/list'>
+          <List />
+        </Route>
+
+        {/* Route for search.js contents */}
+        <Route path='/search'>
+          <Search />
         </Route>
       </div>
     </Router>
