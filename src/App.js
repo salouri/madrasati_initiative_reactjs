8e159a9ece9fa4fd9b00e1740da75c7fbe1dd3bf
Salouri Developer <salouri@gmail.com>
2021-10-12 06:16:38 +0300

    Add the Register component, and import it into App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/navBar';
+import Register from './contents/register';
 
 function App() {
   return (
     <Router>
       <div className='App'>
         <Navbar />
+
+        <Route exact path='/'>
+          <Register />
+        </Route>
       </div>
     </Router>
   );
---
+++ src/contents/register.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+import madrasatipic from '../img/madrasati_logo.png';
+import Social from '../components/social';
+
+class Register extends Component {
+  render() {
+    return (
+      <div className='condiv register'>
+        <img src={madrasatipic} className='profilepic' alt='Madrasati Logo'></img>
+        <ReactTypingEffect
+          className='typingeffect'
+          text={[
+            'Madrasati',
+            'improving the educational environment of Jordan’s most neglected public schools',
+          ]}
+          speed={100}
+          eraseDelay={700}
+          displayTextRenderer={(text, i) => {
+            return i === 0 ? <h1>{text}</h1> : text;
+          }}
+        />
+
+        <Social />
+      </div>
+    );
+  }
+}
+
+export default Register;
