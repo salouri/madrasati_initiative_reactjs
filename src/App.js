861b8000d196250b7081ef746af2b08e54f60e2a
Salouri Developer <salouri@gmail.com>
2021-10-15 09:39:46 +0300

    Install react-bootstrap package and update link to it's css file

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Register from './contents/register';
 import List from './contents/list';
 import Search from './contents/search';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className='App'>
-        <header>Madrasati Single-Page-App Assignment...</header>
+        <header className='App-header'>
+          Madrasati Single-Page-App Assignment...
+        </header>
         <Navbar />
         {/* Route for register.js contents */}
         <Route exact path='/'>
