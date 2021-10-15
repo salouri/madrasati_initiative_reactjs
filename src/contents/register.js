f3d9787af10c99574ba2fb23ea17a742e43005ed
Salouri Developer <salouri@gmail.com>
2021-10-15 03:51:33 +0300

    remove react-typing-effect package/code

--- src/contents/register.js
+++ src/contents/register.js
@@ -1,32 +1,16 @@
 import React, { Component } from 'react';
-import ReactTypingEffect from 'react-typing-effect';
 
-import madrasatipic from '../img/madrasati_logo.png';
 import Social from '../components/social';
 import StudentForm from '../components/studentForm';
 
 class Register extends Component {
   render() {
     return (
-      <div className='condiv register'>
-        <div id='header'>
-          <img src={madrasatipic} className='profilepic' alt='Madrasati Logo'></img>
-          <ReactTypingEffect
-            className='typingeffect'
-            text={[
-              'Madrasati',
-              'improving the educational environment of Jordan’s most neglected public schools',
-            ]}
-            speed={100}
-            eraseDelay={700}
-            displayTextRenderer={(text, i) => {
-              return i === 0 ? <h1>{text}</h1> : text;
-            }}
-          />
-        </div>
+      <div className='condiv'>
         <br />
+        <h2>{'Register a new student:'}</h2>
         <StudentForm />
-
+        <br /> <br />
         <Social />
       </div>
     );
